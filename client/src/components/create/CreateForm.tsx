f7a9597e107dc63ef50e7f6fe16b23d0b9ccc3e3
Amaral, Fabio <fabioamaral.93jr@gmail.com>
2024-06-25 16:26:22 -0300

    adds refetch property and form validation in CreateForm component

--- client/src/components/create/CreateForm.tsx
+++ client/src/components/create/CreateForm.tsx
@@ -4,20 +4,28 @@ import { FieldValues, useForm } from "react-hook-form";
 
 type Props = {
   onSubmit: () => void;
+  refetch: () => void;
 };
 
-const CreateForm = ({ onSubmit }: Props) => {
-  const { register, handleSubmit } = useForm();
+const CreateForm = ({ onSubmit, refetch }: Props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isValid },
+  } = useForm();
   const [{ loading, error }, executePost] = useAxios(
     {
       url: `${process.env.REACT_APP_SERVER_BASE_URL}/users`,
       method: "POST",
     },
-    { manual: true }
+    { manual: true },
   );
 
   const onFormSubmit = async (data: FieldValues) => {
+    if (!isValid) return;
+
     await executePost({ data });
+    refetch();
     onSubmit();
   };
 
@@ -39,14 +47,21 @@ const CreateForm = ({ onSubmit }: Props) => {
           <TextField
             label="First Name"
             variant="outlined"
+            required={true}
             {...register("firstName")}
           />
           <TextField
             label="Last Name"
             variant="outlined"
+            required={true}
             {...register("lastName")}
           />
-          <TextField label="Email" variant="outlined" {...register("email")} />
+          <TextField
+            label="Email"
+            variant="outlined"
+            {...register("email")}
+            required={true}
+          />
           <Button variant="contained" type="submit" disabled={loading}>
             Create User
           </Button>
