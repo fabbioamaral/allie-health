d69dab123db2c42f662b30dfdbd1e5bc592944c5
Amaral, Fabio <fabioamaral.93jr@gmail.com>
2024-06-25 18:29:42 -0300

    displays user details in EditUser component

--- client/src/components/edit/index.tsx
+++ client/src/components/edit/index.tsx
@@ -1,20 +1,32 @@
+import { Link, useParams } from "react-router-dom";
 import { User } from "../../models/user.model";
+import useAxios from "axios-hooks";
+import { Button } from "@mui/material";
 
-type Props = {
-  user?: User;
-};
+const EditUser = () => {
+  let { id } = useParams();
 
-const EditUser = ({ user }: Props) => {
+  const [{ data, loading, error }] = useAxios({
+    url: `${process.env.REACT_APP_SERVER_BASE_URL}/user`,
+    params: { id },
+    method: "GET",
+  });
+
   return (
     <>
+      <Button variant="contained" type="submit" disabled={loading}>
+        <Link to="/" style={{ color: "white", textDecorationLine: "none" }}>
+          Home
+        </Link>
+      </Button>
       <h2>First Name</h2>
-      <p>{user?.firstName}</p>
+      <p>{data?.user.first_name}</p>
       <h2>Last Name</h2>
-      <p>{user?.lastName}</p>
+      <p>{data?.user.last_name}</p>
       <h2>Email</h2>
-      <p>{user?.email}</p>
+      <p>{data?.user.email}</p>
       <h2>Birthday</h2>
-      <p>{user?.birthday}</p>
+      <p>{data?.user.birthday ? data?.user.birthday : "Not Informed"}</p>
     </>
   );
 };
